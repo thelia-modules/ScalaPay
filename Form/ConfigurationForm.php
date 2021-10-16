14749091684cd22cc5deab84176a7f014eb21327
Franck Allimant <fallimant@openstudio.fr>
2021-10-16 02:15:38 +0200

    Fix config form

--- Form/ConfigurationForm.php
+++ Form/ConfigurationForm.php
@@ -1,17 +1,14 @@
 <?php
 
-
 namespace Scalapay\Form;
 
-
-use OpenApi\Constraint\NotBlank;
-use Payline\Payline;
 use Scalapay\Scalapay;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Thelia\Core\Translation\Translator;
 use Thelia\Form\BaseForm;
 
